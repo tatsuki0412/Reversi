3a9010703cdb634a991b1f16c5425180516db588
Serina Sakurai <mickeysnow1031@gmail.com>
2025-04-10 04:46:16 +0900

    Add junit5 for automated testing

---
+++ src/test/java/com/reversi/common/EventSystemTest.java
@@ -0,0 +1,12 @@
+package com.reversi.common;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+public class EventSystemTest {
+  @Test
+  public void testSth() {
+    assertEquals(1 + 1, 2);
+  }
+}
