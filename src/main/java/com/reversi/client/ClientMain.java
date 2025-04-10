3eda87399f4a7b239010de5be931f5a1b2e72769
Serina Sakurai <mickeysnow1031@gmail.com>
2025-04-11 07:29:23 +0900

    bug: fix switching to game panel

--- src/main/java/com/reversi/client/ClientMain.java
+++ src/main/java/com/reversi/client/ClientMain.java
@@ -1,19 +1,31 @@
 package com.reversi.client;
 
 import com.reversi.common.EventBus;
+import com.reversi.common.EventListener;
+import com.reversi.common.Message;
 import javax.swing.SwingUtilities;
 
-public class ClientMain {
-  public static void main(String[] args) {
+public class ClientMain implements EventListener<ServerMessage> {
+  // Entry point
+  public static void main(String[] args) { new ClientMain().start(); }
+
+  EventBus eventBus = new EventBus();
+  LobbyView lobbyView = new LobbyView();
+  GameController controller = new GameController(eventBus);
+
+  private void start() {
     SwingUtilities.invokeLater(() -> {
-      EventBus eventBus = new EventBus();
-      LobbyView lobbyView = new LobbyView();
-      GameController controller = new GameController(eventBus);
       lobbyView.setController(controller);
-      // Register the game view to receive server messages.
       eventBus.register(ServerMessage.class, lobbyView.getGameView());
-      // Connect to the server.
+      eventBus.register(ServerMessage.class, this);
+
       controller.connectToServer();
     });
+  }
+
+  @Override
+  public void onEvent(ServerMessage e) {
+    if (e.getMessage().getType() == Message.Type.Start)
+      lobbyView.switchToGameView();
   }
 }
